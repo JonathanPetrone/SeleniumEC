b98d8d249fe9cc4234913197388a0002e2d7dd9a
Jonathan Petrone <44399523+JonathanPetrone@users.noreply.github.com>
2022-01-12 11:37:51 +0100

    Update SeleniumEC1.cs

--- TestProject/SeleniumEC1.cs
+++ TestProject/SeleniumEC1.cs
@@ -1,6 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using System;
+using System.Threading;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
 
 
 
@@ -12,14 +16,41 @@ namespace SeleniumTestProject
         [TestMethod]
         public void TestMethod1()
         {
-        ChromeDriver driver = new ChromeDriver();
+        ChromeDriver driver = new ChromeDriver(); // startar en ny driver
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
-        driver.Manage().Window.Maximize();
-        driver.Navigate().GoToUrl("http://ecutbildning.se");
+        driver.Manage().Window.Maximize(); // maximerar Fönstret
+        driver.Navigate().GoToUrl("http://ecutbildning.se"); // går till vår URL
 
-        driver.Close();
-		driver.Quit();
+            IWebElement utbElement = driver.FindElement(By.LinkText("Till utbildningarna")); // hittar länken "till utbildningarna"
+            utbElement.Click(); // klickar på länken "till utbildningarna"
+
+            IWebElement cookiesElement = driver.FindElement(By.LinkText("Tillåt alla cookies")); // hittar länken "tillåt alla cookies"
+            cookiesElement.Click(); // klickar på länken "tillåt alla cookies"
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver; // gör så att vi använder oss av JavaScriptExecutor
+            js.ExecuteScript("window.scrollTo(0, 2000)"); // scrollar nedåt
+
+            new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@href='https://ecutbildning.se/utbildningar/mjukvarutestare/' and @class='card-link']"))); // en wait som låter oss vänta till elementet upptäcks
+            js.ExecuteScript("window.scrollTo(0, 2000)"); // scrollar nedåt
+
+            new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@href='https://ecutbildning.se/utbildningar/mjukvarutestare/' and @class='card-link']"))); // en wait som låter oss vänta till elementet upptäcks
+            js.ExecuteScript("window.scrollTo(0, 2000)"); // scrollar nedåt
+
+            IWebElement mvtElement = driver.FindElement(By.XPath("//*[@href='https://ecutbildning.se/utbildningar/mjukvarutestare/' and @class='card-link']")); // hittar länken till utbildningen
+            mvtElement.Click(); // klickar länken till utbildningen
+
+            IWebElement ortElement = driver.FindElement(By.CssSelector("#dropdownMenuLink > span")); // hittar CSS-selectorn med "välj en studieort"
+            ortElement.Click(); // klickar på den
+            IWebElement malmoElement = driver.FindElement(By.LinkText("Malmö")); // hittar "Malmö" i listan
+            malmoElement.Click(); // klickar på Malmö
+
+            IWebElement omfElement = driver.FindElement(By.XPath("/html/body/div[4]/div[3]/div[2]/div/div/div/div[2]/div/div/div/div[2]/p[4]/span[2]")); // hittar texten under omfattning
+            Assert.AreEqual("1,5 år", omfElement.Text); // frågar om det stämmer att texten säger 1,5 år
+
+            driver.Close();
+        
+        driver.Quit();
 
         }
     }
