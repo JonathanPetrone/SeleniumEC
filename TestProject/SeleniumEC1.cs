4d38f3ada057159335210dc83cd02f2867178da9
Jonathan Petrone <petrone1993@gmail.com>
2022-02-10 07:25:32 +0100

    Uoppdaterade med kodexempel

--- TestProject/SeleniumEC1.cs
+++ TestProject/SeleniumEC1.cs
@@ -1,7 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using System;
-
+using System.Threading;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
 
 
 namespace SeleniumTestProject
@@ -21,6 +24,40 @@ namespace SeleniumTestProject
         driver.Close();
 		driver.Quit();
 
+        // Olika typer av metoder
+
+        /* 
+        
+        Exempel:
+
+        (Hitta något med hjälp av LinkText)
+        IWebElement utbKnapp = driver.FindElement(By.LinkText("Våra utbildningar")); // Steg 1 vi pekar ut knappen med länktexten "Våra utbildningar" och ger den namnet utbKnapp
+        utbKnapp.Click(); // Steg 2 vi klickar på knappen som vi gjorde i tidigare steg
+
+        ----OLIKA LOCATORS----
+
+        (Du behöver byta namnet "ElementetsNamn")
+
+        IWebElement ElementetsNamn = driver.FindElement(By.ClassName("Class-namnet skrivs här"));
+        IWebElement ElementetsNamn = driver.FindElement(By.CssSelector("CSS-selectorn skrivs här"));
+        IWebElement ElementetsNamn = driver.FindElement(By.Id("ID:t skrivs här"));
+        IWebElement ElementetsNamn = driver.FindElement(By.LinkText("Länktexten skrivs här"));
+        IWebElement ElementetsNamn = driver.FindElement(By.Name("Namnet skrivs här"));
+        IWebElement ElementetsNamn = driver.FindElement(By.PartialLinkText("Del av länktexten skrivs här"));
+        IWebElement ElementetsNamn = driver.FindElement(By.TagName("Taggnamnet skrivs här"));
+        IWebElement ElementetsNamn = driver.FindElement(By.Xpath(string xpath));
+
+        ----OLIKA KOMMANDON----
+
+        element.Click();
+
+        ----OLIKA ASSERTS----
+
+        EXEMPEL: 
+        Assert.AreEqual("1,5 år", ElementetsNamn.Text); // frågar om det stämmer att texten säger 1,5 år
+
+        */
+        
         }
     }
 }
